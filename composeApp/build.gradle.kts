e7152c1c42993f9d0036a78385901adb47480711
Rui Vieira <revs87@msn.com>
2024-08-14 03:11:53 +0100

    Build fixes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,6 +72,7 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.viewmodel.compose)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.mob.settings)
             implementation(libs.kotlin.coroutines)
