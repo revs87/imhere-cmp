65bbd8ab9a136c6aae490ad22a1ed7bd82aabf35
Rui Vieira <revs87@msn.com>
2024-09-05 00:10:11 +0100

    UI updates

--- composeApp/src/commonMain/kotlin/com/rvcoding/imhere/App.kt
+++ composeApp/src/commonMain/kotlin/com/rvcoding/imhere/App.kt
@@ -1,22 +1,13 @@
 package com.rvcoding.imhere
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialogDefaults.containerColor
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.rvcoding.imhere.di.IHKoin
 import com.rvcoding.imhere.ui.screens.allinoneapi.AllInOneApiScreen
@@ -44,27 +35,7 @@ fun App() {
                     .padding(innerPadding),
             ) {
 
-
-                var showContent by remember { mutableStateOf(false) }
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button(onClick = { showContent = !showContent }) {
-                        Text("Fetch Users")
-                    }
-                    AnimatedVisibility(showContent) {
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text("Registered users:")
-                            //UsersScreen()
-                            AllInOneApiScreen()
-                        }
-                    }
-                }
-
+                AllInOneApiScreen()
 
             }
         }
--- composeApp/src/commonMain/kotlin/com/rvcoding/imhere/ui/screens/allinoneapi/AllInOneApiScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rvcoding/imhere/ui/screens/allinoneapi/AllInOneApiScreen.kt
@@ -1,7 +1,9 @@
 package com.rvcoding.imhere.ui.screens.allinoneapi
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -14,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.rvcoding.imhere.ui.screens.users.UsersScreen
 import com.rvcoding.imhere.ui.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.getKoin
@@ -27,26 +30,38 @@ fun AllInOneApiScreen(
 ) {
     val content by sm.content.collectAsStateWithLifecycle()
     var userId by remember { mutableStateOf("revs") }
+    var showUsers by remember { mutableStateOf(false) }
 
     AppTheme {
         Box(
             modifier = modifier,
             contentAlignment = Alignment.Center
         ) {
-            Column {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = { sm.requestConfiguration(); showUsers = false }) { Text("Configuration") }
                 TextField(
                     value = userId,
                     onValueChange = { userId = it },
                     label = { Text("User ID") }
                 )
-                Button(onClick = { sm.requestConfiguration() }) { Text("Configuration") }
-                Button(onClick = { sm.requestRegister(userId, "test", "", "") }) { Text("Register") }
-                Button(onClick = { sm.requestLogin(userId, "test") }) { Text("Login") }
-                Button(onClick = { sm.requestUsers() }) { Text("Users") }
-                Text(
-                    modifier = Modifier.fillMaxSize(),
-                    text = content
-                )
+                Row {
+                    Button(onClick = { sm.requestRegister(userId, "test", "", ""); showUsers = false }) { Text("Register") }
+                    Button(onClick = { sm.requestLogin(userId, "test"); showUsers = false }) { Text("Login") }
+                    Button(onClick = { /*sm.requestUsers()*/ showUsers = true }) { Text("Users") }
+                }
+
+                AnimatedVisibility(showUsers) {
+                    UsersScreen()
+                }
+                AnimatedVisibility(!showUsers) {
+                    Text(
+                        modifier = Modifier.fillMaxSize(),
+                        text = content
+                    )
+                }
             }
 
         }
