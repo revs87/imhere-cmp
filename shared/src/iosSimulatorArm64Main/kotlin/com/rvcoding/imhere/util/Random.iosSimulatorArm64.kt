8956ad5b1b6067adac8e3ff1c9d2ca8a68c652b6
Rui Vieira <revs87@msn.com>
2025-06-18 00:11:12 +0100

    Multiplatform Building issues fixed

--- shared/src/iosSimulatorArm64Main/kotlin/com/rvcoding/imhere/util/Random.iosSimulatorArm64.kt
+++
@@ -1,7 +0,0 @@
-package com.rvcoding.imhere.util
-
-import platform.Foundation.NSUUID
-
-actual fun randomUUID(): UUID = iOSSimulatorUUID(NSUUID().UUIDString())
-
-data class iOSSimulatorUUID(override val uuid: String) : UUID
