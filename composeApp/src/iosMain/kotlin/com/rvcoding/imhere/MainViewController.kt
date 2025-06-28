74b74fa6cf7d1d9b8e4496f36d3b4785556c1e40
Rui Vieira <revs87@msn.com>
2025-06-29 00:51:58 +0100

    iOS UI parallelRendering turned ON + Lib versions updated

--- composeApp/src/iosMain/kotlin/com/rvcoding/imhere/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/rvcoding/imhere/MainViewController.kt
@@ -1,11 +1,14 @@
 package com.rvcoding.imhere
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeUIViewController
 import com.rvcoding.imhere.di.appModule
 import com.rvcoding.imhere.di.initKoin
 
+@ExperimentalComposeUiApi
 fun MainViewController() = ComposeUIViewController(
     configure = {
+        parallelRendering = true
         initKoin(
             appModules = listOf(appModule)
         )
